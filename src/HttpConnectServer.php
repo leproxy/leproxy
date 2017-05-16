9929971df9f7027ea2234577347f060d57fe82d2
Christian Lück <christian@lueck.tv>
2017-05-16 17:25:21 +0200

    Simplify request handler to ease handling other requests in the future

--- src/HttpConnectServer.php
+++ src/HttpConnectServer.php
@@ -11,7 +11,8 @@ class HttpConnectServer
 {
     public function __construct(LoopInterface $loop, ServerInterface $socket, ConnectorInterface $connector)
     {
-        $server = new \React\Http\Server($socket, function (ServerRequestInterface $request) use ($connector) {
+        $that = $this;
+        $server = new \React\Http\Server($socket, function (ServerRequestInterface $request) use ($connector, $that) {
             if ($request->getMethod() !== 'CONNECT') {
                 return new Response(
                     405,
@@ -20,50 +21,56 @@ class HttpConnectServer
                 );
             }
 
-            // pause consuming request body
-            $body = $request->getBody();
-            $body->pause();
-
-            $buffer = '';
-            $body->on('data', function ($chunk) use (&$buffer) {
-                $buffer .= $chunk;
-            });
-
-            // try to connect to given target host
-            $promise = $connector->connect($request->getRequestTarget())->then(
-                function (ConnectionInterface $remote) use ($body, &$buffer) {
-                    // connection established => forward data
-                    $body->pipe($remote);
-                    $body->resume();
-
-                    if ($buffer !== '') {
-                        $remote->write($buffer);
-                        $buffer = '';
-                    }
-
-                    return new Response(
-                        200,
-                        array(),
-                        $remote
-                    );
-                },
-                function ($e) {
-                    return new Response(
-                        502,
-                        array('Content-Type' => 'text/plain'),
-                        'Unable to connect: ' . $e->getMessage()
-                    );
-                }
-            );
-
-            // cancel pending connection if request closes prematurely
-            $body->on('close', function () use ($promise) {
-                $promise->cancel();
-            });
-
-            return $promise;
+            return $that->handleConnectRequest($request, $connector);
         });
 
         $server->on('error', 'printf');
+    }
+
+    /** @internal */
+    public function handleConnectRequest(ServerRequestInterface $request, ConnectorInterface $connector)
+    {
+        // pause consuming request body
+        $body = $request->getBody();
+        $body->pause();
+
+        $buffer = '';
+        $body->on('data', function ($chunk) use (&$buffer) {
+            $buffer .= $chunk;
+        });
+
+        // try to connect to given target host
+        $promise = $connector->connect($request->getRequestTarget())->then(
+            function (ConnectionInterface $remote) use ($body, &$buffer) {
+                // connection established => forward data
+                $body->pipe($remote);
+                $body->resume();
+
+                if ($buffer !== '') {
+                    $remote->write($buffer);
+                    $buffer = '';
+                }
+
+                return new Response(
+                    200,
+                    array(),
+                    $remote
+                    );
+            },
+            function ($e) {
+                return new Response(
+                    502,
+                    array('Content-Type' => 'text/plain'),
+                    'Unable to connect: ' . $e->getMessage()
+                );
+            }
+        );
+
+        // cancel pending connection if request closes prematurely
+        $body->on('close', function () use ($promise) {
+            $promise->cancel();
+        });
+
+        return $promise;
     }
 }
