075c456a9009b791a69df4a57120f1d34fbfccda
Christian Lück <christian@lueck.tv>
2017-06-21 23:02:17 +0200

    Refactor to use single parsed array of $args

--- leproxy.php
+++ leproxy.php
@@ -4,35 +4,30 @@ use Clue\Commander\Router;
 
 require __DIR__ . '/vendor/autoload.php';
 
-$listen = '127.0.0.1:1080';
-$path = array();
-
 // parse options from command line arguments (argv)
 $commander = new Router();
-$commander->add('[<listen> [<forward>...]]', function ($args) use (&$listen, &$path) {
-    if (isset($args['listen'])) {
-        $listen = $args['listen'];
-    }
-    if (isset($args['forward'])) {
-        $path = $args['forward'];
-    }
+$commander->add('[<listen> [<path>...]]', function ($args) {
+    return $args + array(
+        'listen' => '127.0.0.1:1080',
+        'path' => array()
+    );
 });
-$commander->execArgv();
+$args = $commander->handleArgv();
 
 $loop = React\EventLoop\Factory::create();
 
 // set next proxy server chain -> p1 -> p2 -> p3 -> destination
-$connector = ConnectorFactory::createConnectorChain($path, $loop);
+$connector = ConnectorFactory::createConnectorChain($args['path'], $loop);
 
 // listen on 127.0.0.1:1080 or first argument
 $proxy = new LeProxyServer($loop, $connector);
-$socket = $proxy->listen($listen);
+$socket = $proxy->listen($args['listen']);
 
 $addr = str_replace('tcp://', 'http://', $socket->getAddress());
 echo 'LeProxy HTTP/SOCKS proxy now listening on ' . $addr . PHP_EOL;
 
-if ($path) {
-    echo 'Forwarding via: ' . implode(' -> ', $path) . PHP_EOL;
+if ($args['path']) {
+    echo 'Forwarding via: ' . implode(' -> ', $args['path']) . PHP_EOL;
 }
 
 $loop->run();
